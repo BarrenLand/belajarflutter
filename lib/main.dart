f3a4990faed11638abf8dd60795c6e201e623380
BarrenLand <danielkevinlangley@live.com>
2022-04-17 14:33:16 +0700

    update_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:belajarflutter/ui/produk_form.dart';
+import 'package:belajarflutter/ui/produk_page.dart';
+import 'package:belajarflutter/ui/produk_form.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ProdukPage());
+  runApp(const MyApp());
 }
 
-class ProdukPage extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aplikasi Flutter Pertama',
-      home: ProdukForm(),
+      home: Scaffold(
+        body: ProdukPage(),
+      ),
     );
   }
 }
